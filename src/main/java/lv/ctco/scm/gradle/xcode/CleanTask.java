1db5d953608515bb5cb707bab8e0973a561afeb2
Ivars Berzinsh <ivars.berzinsh@ctco.lv>
2023-01-17 13:16:01 +0200

    Removed PathUtil references from Xcode CleanTask

--- src/main/java/lv/ctco/scm/gradle/xcode/CleanTask.java
+++ src/main/java/lv/ctco/scm/gradle/xcode/CleanTask.java
@@ -8,13 +8,13 @@ package lv.ctco.scm.gradle.xcode;
 
 import lv.ctco.scm.mobile.utils.BackupUtil;
 import lv.ctco.scm.gradle.utils.ErrorUtil;
-import lv.ctco.scm.mobile.utils.PathUtil;
 
 import org.apache.commons.io.FileUtils;
 
 import org.gradle.api.DefaultTask;
 import org.gradle.api.tasks.TaskAction;
 
+import java.io.File;
 import java.io.IOException;
 
 public class CleanTask extends DefaultTask {
@@ -23,11 +23,11 @@ public class CleanTask extends DefaultTask {
     public void doTaskAction() {
         try {
             BackupUtil.restoreAllFiles();
-            FileUtils.deleteDirectory(PathUtil.getBackupDir());
-            FileUtils.deleteDirectory(PathUtil.getXcodeDstDir());
-            FileUtils.deleteDirectory(PathUtil.getXcodeObjDir());
-            FileUtils.deleteDirectory(PathUtil.getXcodeSymDir());
-            FileUtils.deleteDirectory(PathUtil.getXcodeSharedDir());
+            FileUtils.deleteDirectory(new File(getProject().getBuildDir(), "backups"));
+            FileUtils.deleteDirectory(new File(getProject().getBuildDir(), "xcodebuild/dst"));
+            FileUtils.deleteDirectory(new File(getProject().getBuildDir(), "xcodebuild/obj"));
+            FileUtils.deleteDirectory(new File(getProject().getBuildDir(), "xcodebuild/sym"));
+            FileUtils.deleteDirectory(new File(getProject().getBuildDir(), "xcodebuild/shared"));
         } catch (IOException e) {
             ErrorUtil.errorInTask(this.getName(), e);
         }
